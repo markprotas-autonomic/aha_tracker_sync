2c21ade10fe1da926113e444d510920bd30caee7
Mark Protas <mark.protas@autonomic.ai>
2019-11-07 14:41:42 -0500

    Don't create releases in Aha if they already exist (name as pk).  Utilize jq to query for specific elements rather than a more fragile regex

--- aha_tracker_sync.sh
+++ aha_tracker_sync.sh
@@ -8,8 +8,16 @@ if [ -z "$TRACKER_TOKEN" ] || [ -z "$AHA_TOKEN" ] || [ -z "$TRACKER_PROJECT_ID" 
   display_usage
 fi
 
-releases=`curl -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/2167812/stories?with_story_type=release&with_state=planned" | jq . | grep name | awk '{ s = ""; for (i = 2; i <= NF; i++) s = s $i " "; print s }'`
-IFS=','
+existing_aha_releases=`curl -s -H "Authorization: Bearer $AHA_TOKEN" https://autonomic2.aha.io/api/v1/products/$AHA_PRODUCT_ID/releases`
+releases=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/2167812/stories?with_story_type=release&with_state=planned&fields=name" | jq -r ".[].name"`
+IFS=$'\n'
 for release in $releases; do
-  curl -XPOST -H "Authorization: Bearer $AHA_TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"release\":{\"name\":${release}}}" https://autonomic2.aha.io/api/v1/products/$AHA_PRODUCT_ID/releases
+  release_name_query_results=`echo $existing_aha_releases | jq -e ".releases[] | select(.name == \"$release\")"`
+  if [[ $? == 0 ]]; then
+    echo "Release '$release' already exists in Aha"
+    continue
+  else
+    echo "Creating release '$release' already exists in Aha"
+    curl -XPOST -H "Authorization: Bearer $AHA_TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"release\":{\"name\":\"$release\"}}" https://autonomic2.aha.io/api/v1/products/$AHA_PRODUCT_ID/releases
+  fi
 done
